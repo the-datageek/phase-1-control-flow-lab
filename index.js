fdbd0e2cde526f5d94408e87a3471863c5d9e9b0
Faith Moraa <moraaf770@gmail.com>
2022-11-23 02:34:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride<=400){ride
+    return "This one is on me!";
+  }else if (ride>2000 && ride<2500){
+    return "I will gladly take your thirty bucks.";
+  }else if(ride > 2500){
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(city){
+  if(city === "NYC") {
+    return "Ok, sounds good.";
+  }
+  else{
+    return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+   if(tip==="generous"){
+    return "Thank you so much.";
+   }else if (tip === "not as generous"){
+    return "Thank you.";
+   }else {
+    return "Bye.";
+   }
 }
